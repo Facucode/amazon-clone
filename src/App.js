740e7e9f317bad882bc07b5495cba1371601c61d
Facucode <34733879+Facucode@users.noreply.github.com>
2021-05-16 01:25:29 -0300

    Home and Checkout creation

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import React from 'react'
+import Header from './Header'
+import Home from "./Home"
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Checkout from './Checkout';
+
+function App() {
+  return (
+    //BEM
+    <Router>
+    <div className="App">
+      <Switch>
+    
+     <Route path="/checkout">
+     <Header/>
+     <Checkout/>
+     </Route>
+     <Route path="/">
+     <Header/>
+     <Home/>
+     </Route>
+    
+
+     </Switch>
+    </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAMM6Ir1Yx6bQrhtKAudgIGqy3V9LniLe4",
+    authDomain: "clone-97d43.firebaseapp.com",
+    projectId: "clone-97d43",
+    storageBucket: "clone-97d43.appspot.com",
+    messagingSenderId: "804269293644",
+    appId: "1:804269293644:web:86a381e5b8cc480da774e9",
+    measurementId: "G-PTSFYKM9MJ"
+  };
